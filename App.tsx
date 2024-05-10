5b5ae38aa44e7fa7d1812b204d7f138cdc4c43ef
matheusdonizetiferreira <mathferreira726@gmail.com>
2024-05-10 19:57:56 -0300

    projeto my app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
+import { styles } from './styles'
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>
+        Escolha {"\n"}
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+      <Text style={styles.message}>
+        Descubra receitas basedas nos produtos que você escolheu.
+      </Text>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles.ts
@@ -0,0 +1,27 @@
+import {StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#999',
+      padding:24
+    //  alignItems: 'center',
+     // justifyContent: 'center',
+    },
+    title:{
+        fontSize:36,
+        fontFamily: 'bold',
+        lineHeight: 44,
+        marginTop: 42
+    },
+    subtitle:{
+
+    },
+    message:{
+        fontSize: 20,
+        color: "#7C7C8A",
+        marginTop: 12,
+        marginBottom: 38
+    }
+
+  });
